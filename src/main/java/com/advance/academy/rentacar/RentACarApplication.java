9ce6c492cb73d4d00ad793198f04612a6468f183
Ivan <iwolkkov@gmail.com>
2024-05-22 21:02:02 +0300

    use mysql server

--- src/main/java/com/advance/academy/rentacar/RentACarApplication.java
+++ src/main/java/com/advance/academy/rentacar/RentACarApplication.java
@@ -1,8 +1,6 @@
 package com.advance.academy.rentacar;
 
-import com.advance.academy.rentacar.entity.Car;
-import com.advance.academy.rentacar.service.CarsService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
